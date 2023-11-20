8cf480199172ffeb011faa8d38467e2054511679
zhang <2210439454@qq.com>
2023-11-20 16:50:58 +0800

    展示角色数据

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -6,12 +6,24 @@
         <el-button size="mini" type="primary">添加角色</el-button>
       </div>
       <!-- 放置table组件 -->
-      <el-table>
+      <el-table :data="list">
         <!-- 放置列 -->
-        <el-table-column align="center" width="200" label="角色" />
-        <el-table-column align="center" width="200" label="启用" />
-        <el-table-column align="center" label="描述" />
-        <el-table-column align="center" label="操作" />
+        <el-table-column prop="name" align="center" width="200" label="角色" />
+        <el-table-column prop="state" align="center" width="200" label="启用">
+          <!-- 自定义列结构 -->
+          <template v-slot="{ row }">
+            <span>{{ row.state === 1 ? "已启用" : row.state === 0 ? "未启用" : "无" }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="description" align="center" label="描述" />
+        <el-table-column align="center" label="操作">
+          <!-- 放置操作按钮 -->
+          <template>
+            <el-button size="mini" type="text">分配权限</el-button>
+            <el-button size="mini" type="text">编辑</el-button>
+            <el-button size="mini" type="text">删除</el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <!-- 放置分页组件 -->
       <el-row type="flex" style="height:60px" align="middle" justify="end">
@@ -21,8 +33,24 @@
   </div>
 </template>
 <script>
+import { getRoleList } from '@/api/role'
+
 export default {
-  name: 'Role'
+  name: 'Role',
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.getRoleList()
+  },
+  methods: {
+    async getRoleList() {
+      const { rows } = await getRoleList()
+      this.list = rows // 赋值数据
+    }
+  }
 }
 </script>
 <style scoped>
