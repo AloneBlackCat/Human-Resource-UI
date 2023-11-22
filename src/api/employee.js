14d54c75e1a89af9ebde06f840c990df6d91c3ad
zhang <2210439454@qq.com>
2023-11-22 16:11:24 +0800

    员工导入模板的下载

--- src/api/employee.js
+++ src/api/employee.js
@@ -16,3 +16,11 @@ export function exportEmployee() {
     responseType: 'blob' // 使用blob类型接收二进制文件流
   })
 }
+
+// 下载员工导入模板
+export function getExportTemplate() {
+  return request({
+    url: '/sys/user/import/template',
+    responseType: 'blob' // 二进制文件流
+  })
+}
