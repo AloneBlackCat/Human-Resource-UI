72d6442a77e0ab7f5ec8f7121c2e5870c33f4ab0
zhang <2210439454@qq.com>
2023-11-22 16:02:24 +0800

    excel导入组件的封装

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -26,7 +26,7 @@
       <div class="right">
         <el-row class="opeate-tools" type="flex" justify="end">
           <el-button size="mini" type="primary">添加员工</el-button>
-          <el-button size="mini">excel导入</el-button>
+          <el-button size="mini" @click="showExcelDialog = true">excel导入</el-button>
           <el-button size="mini" @click="exportEmployee">excel导出</el-button>
         </el-row>
         <!-- 表格组件 -->
@@ -69,6 +69,8 @@
         </el-row>
       </div>
     </div>
+    <!-- 放置导入组件 -->
+    <import-excel :show-excel-dialog.sync="showExcelDialog" />
   </div>
 </template>
 
@@ -77,9 +79,13 @@ import { getDepartment } from '@/api/department'
 import { transListToTreeData } from '@/utils/index'
 import { getEmployeeList, exportEmployee } from '@/api/employee'
 import FileSaver from 'file-saver'
+import ImportExcel from './components/import-excel.vue'
 
 export default {
   name: 'Employee',
+  components: {
+    ImportExcel
+  },
   data() {
     return {
       depts: [], // 组织数据
@@ -95,7 +101,8 @@ export default {
         keyword: ''
       },
       total: 0, // 记录员工的总数
-      list: [] // 存储员工列表数据
+      list: [], // 存储员工列表数据
+      showExcelDialog: false // 控制excel的单层显示和隐藏
     }
   },
   created() {
