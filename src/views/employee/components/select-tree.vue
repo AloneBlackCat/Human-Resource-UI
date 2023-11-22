63dc5751c529898fc4d24696ac9b1e54cfd9504b
aloneBlackCat <2210439454@qq.com>
2023-11-22 21:18:53 +0800

    级联树组件的封装

---
+++ src/views/employee/components/select-tree.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- element-ui级联组件 -->
+  <el-cascader
+    size="mini"
+    :options="treeData"
+    :props="props"
+    separator="-"
+  />
+</template>
+
+<script>
+import { getDepartment } from '@/api/department'
+import { transListToTreeData } from '@/utils'
+export default {
+  data() {
+    return {
+      treeData: [], // 赋值给级联组件options
+      props: {
+        label: 'name', // 要展示的字段
+        value: 'id' // 要存储的字段
+      }
+    }
+  },
+  created() {
+    this.getDepartment()
+  },
+  methods: {
+    async getDepartment() {
+      this.treeData = transListToTreeData(await getDepartment(), 0) // 将组织架构数据转化树形数值给treeData
+    }
+  }
+}
+</script>
