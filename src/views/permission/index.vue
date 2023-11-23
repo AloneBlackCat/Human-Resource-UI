2881e30f3316eca41c9dbc423f85b7f39a4d8856
zhang <2210439454@qq.com>
2023-11-23 16:47:25 +0800

    权限页面的布局及数据展示

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,12 +1,46 @@
 <template>
   <div class="container">
     <div class="app-container">
-      权限管理
+      <el-button class="btn-add" size="mini" type="primary">添加权限</el-button>
+      <el-table :data="list" row-key="id" default-expand-all>
+        <el-table-column prop="name" align="center" label="名称" />
+        <el-table-column prop="code" align="center" label="标识" />
+        <el-table-column prop="description" align="center" label="描述" />
+        <el-table-column align="center" label="操作">
+          <template v-slot="{ row }">
+            <el-button v-if="row.type === 1" size="mini" type="text">添加</el-button>
+            <el-button size="mini" type="text">编辑</el-button>
+            <el-button size="mini" type="text">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
 <script>
+import { getPermissionList } from '@/api/permission'
+import { transListToTreeData } from '@/utils'
 export default {
-  name: 'Permission'
+  name: 'Permission',
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.getPermissionList()
+  },
+  methods: {
+    async getPermissionList() {
+      const result = transListToTreeData(await getPermissionList(), 0) // 转换为树形结构
+      this.list = result
+    }
+  }
 }
 </script>
+
+<style scoped>
+.bth-add {
+  margin: 10px;
+}
+</style>
